68349c1c8fa9d19a9c452954400c49f8a334fdb3
Gunjan Gupta <gunjan@wesion.com>
2025-03-31 12:04:54 +0530

    Compress images with metadata for OOWOW
    Signed-off-by: Gunjan Gupta <gunjan@wesion.com>

--- buildroot-external-khadas/board/khadas/vim3l/hassos-hook.sh
+++ buildroot-external-khadas/board/khadas/vim3l/hassos-hook.sh
@@ -11,8 +11,24 @@ function hassos_pre_image() {
     cp "${BOARD_DIR}/cmdline.txt" "${BOOT_DATA}/cmdline.txt"
 }
 
+function convert_disk_image_xze() {
+    local hdd_ext=${1:-img}
+    local hdd_img
+    local board_name="${BOARD_NAME##* }"
+    hdd_img="$(hassos_image_name "${hdd_ext}")"
+
+    rm -f "${hdd_img}.xz"
+    "${BR2_EXTERNAL_KHADAS_PATH}"/scripts/xze "${hdd_img}" \
+        --meta \
+        LABEL="${HASSOS_NAME}" \
+        BOARD="${board_name}" \
+        BUILDER="khadas_haos" \
+        LINK="https://dl.khadas.com/products/oowow/images/${board_name,,}" \
+        DESC="${HASSOS_NAME} - v${VERSION_MAJOR}.${VERSION_MINOR}${VERSION_SUFFIX:+-}${VERSION_SUFFIX}" \
+        > "${hdd_img}.xz"
+}
 
 function hassos_post_image() {
-    convert_disk_image_xz
+    convert_disk_image_xze
 }
 
